96eb18e709066f229acd0498eabb46ad2334f48c
Mario Bravo Pineda <135020003+MarioBravoP@users.noreply.github.com>
2025-05-19 18:35:16 +0200

    Añadido proyecto local

---
+++ src/components/loader/Loader.js
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className={"loader"}>
+      <div className={"loader_spinner"}></div>
+    </div>
+  );
+};
+
+export default Loader;
---
+++ src/context/SliderContext.js
@@ -0,0 +1,32 @@
+"use client";
+import { createContext, useState, useContext } from "react";
+
+const SliderContext = createContext();
+
+export function SliderProvider({ children }) {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [pics, setPics] = useState([]);
+
+  const initializePics = (newPics) => {
+    setPics(newPics);
+    setCurrentIndex(0);
+  };
+
+  const nextSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % pics.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? pics.length - 1 : prevIndex - 1));
+  };
+
+  return (
+    <SliderContext.Provider value={{ currentIndex, nextSlide, prevSlide, initializePics, pics, setPics }}>
+      {children}
+    </SliderContext.Provider>
+  );
+}
+
+export function useSlider() {
+  return useContext(SliderContext);
+}
